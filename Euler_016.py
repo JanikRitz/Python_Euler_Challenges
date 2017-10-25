45a3af1b91c0032d3722d813e8d3eda79c78d10a
JanikRitz <janikritz@gmail.com>
2017-10-25 15:42:37 +0200

    Added Problem 16 with basic variables and output

--- Euler_016.py
+++ Euler_016.py
@@ -1,7 +1,10 @@
 '''
-215 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+If the numbers 1 to 5 are written out in words: one, two, three, four, five, then there are 3 + 3 + 5 + 4 + 4 = 19 letters used in total.
 
-What is the sum of the digits of the number 2^1000?
+If all the numbers from 1 to 1000 (one thousand) inclusive were written out in words, how many letters would be used?
+
+
+NOTE: Do not count spaces or hyphens. For example, 342 (three hundred and forty-two) contains 23 letters and 115 (one hundred and fifteen) contains 20 letters. The use of "and" when writing out numbers is in compliance with British usage.
 '''
 
 import datetime
@@ -9,12 +12,26 @@ import datetime
 start_time = datetime.datetime.now()
 
 # Actual code
-power = 1000
+number_chars = 0
+start = 1
+end = 1000
 
-value = 2**power
-sum_digits = sum([int(i) for i in str(value)])
+number_lengths ={
+    1: 3,
+    2: 3,
+    3: 5,
+    4: 4,
+    5: 4,
+    6: 3,
+    7: 5,
+    8: 5,
+    9: 4,
+    10: 3,
+# ...
+}
 
 # End actual code
 end_time = datetime.datetime.now()
-print(f'There sum of the digits of 2^{power} is {sum_digits} from {value} ({(end_time - start_time)})')
+print(f'When spelled the numbers from {start} to {end} use {number_chars} characters'
+      f'(without spaces and hyphens) ({(end_time - start_time)})')
 # There sum of the digits of 2^1000 is 1366 from ... (0:00:00.000999)
