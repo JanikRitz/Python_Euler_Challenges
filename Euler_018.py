578b89c04e766657e1bc6b6ab87e7d689fcd83e2
JanikRitz <janikritz@gmail.com>
2017-10-26 16:07:05 +0200

    Faster through tuples and caching

--- Euler_018.py
+++ Euler_018.py
@@ -35,30 +35,39 @@ start_time = datetime.datetime.now()
 
 # Actual code
 triangle = list()
+max_routes = dict()
 with open('Numbers_18.txt') as file:
     for line in file:
-        triangle.append(list(map(lambda x: int(x), str(line).split(' '))))
+        triangle.append(tuple(map(lambda x: int(x), str(line).split(' '))))
 
+triangle = tuple(triangle)
 
 def max_route(triangle) -> int:
     if len(triangle) == 1:
         return triangle[0][0]
 
-    return triangle[0][0] + max(max_route(left_sub_triangle(triangle)), max_route(right_sub_triangle(triangle)))
+    if triangle in max_routes:
+        return max_routes[triangle]
+
+    left = max_route(left_sub_triangle(triangle))
+    right = max_route(right_sub_triangle(triangle))
+    max_routes[triangle] = triangle[0][0] + max(left, right)
+
+    return max_routes[triangle]
 
 
 def left_sub_triangle(triangle):
     sub_triangle = list()
     for row in triangle[1:]:
         sub_triangle.append(row[:-1])
-    return sub_triangle
+    return tuple(sub_triangle)
 
 
 def right_sub_triangle(triangle):
     sub_triangle = list()
     for row in triangle[1:]:
         sub_triangle.append(row[1:])
-    return sub_triangle
+    return tuple(sub_triangle)
 
 
 max_total = max_route(triangle)
@@ -66,5 +75,5 @@ max_total = max_route(triangle)
 # End actual code
 end_time = datetime.datetime.now()
 print(f'Maximum Total from top to bottom is {max_total} ({(end_time - start_time)})')
-# Maximum Total from top to bottom is 1074 (0:00:00.027598)
+# Maximum Total from top to bottom is 1074 (0:00:00.000501)
 
