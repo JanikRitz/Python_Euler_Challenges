043a3e36ba32c0f09acb5e4e2d44c0eeb7493c32
JanikRitz <janikritz@gmail.com>
2017-10-26 13:58:15 +0200

    Solved Euler 17

--- Euler_017.py
+++ Euler_017.py
@@ -16,19 +16,79 @@ number_chars = 0
 start = 1
 end = 1000
 
-number_lengths ={
-    1: 3,
-    2: 3,
-    3: 5,
-    4: 4,
-    5: 4,
-    6: 3,
-    7: 5,
-    8: 5,
-    9: 4,
-    10: 3,
-# ...
+numbers_spec = {
+    10: 'Ten',
+    11: 'Eleven',
+    12: 'Twelve',
+    13: 'Thirteen',
+    14: 'Fourteen',
+    15: 'Fifteen',
+    16: 'Sixteen',
+    17: 'Seventeen',
+    18: 'Eighteen',
+    19: 'Nineteen',
 }
+
+numbers_1 = {
+    1: 'One',
+    2: 'Two',
+    3: 'Three',
+    4: 'Four',
+    5: 'Five',
+    6: 'Six',
+    7: 'Seven',
+    8: 'Eight',
+    9: 'Nine',
+}
+
+numbers_2 = {
+    2: 'Twenty',
+    3: 'Thirty',
+    4: 'Forty',
+    5: 'Fifty',
+    6: 'Sixty',
+    7: 'Seventy',
+    8: 'Eighty',
+    9: 'Ninety',
+}
+
+
+def number_as_text(number: int) -> str:
+    if number in numbers_spec:
+        return numbers_spec[number]
+
+    if len(str(number)) == 4:
+        extra = ''
+        if not int(str(number)[1:]) == 0:
+            extra = f' and {number_as_text(int(str(number)[1:]))}'
+        return f'{number_as_text(int(str(number)[0]))} thousand{extra}'
+
+    if len(str(number)) == 3:
+        extra = ''
+        if not int(str(number)[1:]) == 0:
+            extra = f' and {number_as_text(int(str(number)[1:]))}'
+        return f'{number_as_text(int(str(number)[0]))} hundred{extra}'
+
+    if len(str(number)) == 2:
+        extra = ''
+        if not int(str(number)[1:]) == 0:
+            extra = f'-{number_as_text(int(str(number)[1:]))}'
+        return f'{numbers_2[int(str(number)[0])]}{extra}'
+
+    if len(str(number)) == 1:
+        return numbers_1[number]
+
+    raise ValueError
+
+
+def length_number(number: str) -> int:
+    number = number.replace(' ', '')
+    number = number.replace('-', '')
+    return len(number)
+
+
+for i in range(start, end + 1):
+    number_chars += length_number(number_as_text(i))
 
 # End actual code
 end_time = datetime.datetime.now()
