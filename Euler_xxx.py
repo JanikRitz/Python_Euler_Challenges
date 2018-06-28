3ef4a34a3041c45753b2bfa3b7aa31cb04f010bb
JanikRitz <janikritz@gmail.com>
2018-06-28 19:09:46 +0200

    Added template

---
+++ Euler_xxx.py
@@ -0,0 +1,16 @@
+'''
+Problem #xxx
+'''
+
+import datetime
+
+start_time = datetime.datetime.now()
+
+# Actual code
+result = "Result"
+
+# End actual code
+end_time = datetime.datetime.now()
+
+print(f'Result is {result} ({(end_time - start_time)})')
+# Result
