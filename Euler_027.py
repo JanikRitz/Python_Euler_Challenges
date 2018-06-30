7f9b456cee8287e69afe039efc98002ea2f31ef8
JanikRitz <janikritz@gmail.com>
2018-06-30 10:49:05 +0200

    Solved problem #27 with use of primes and caching

--- Euler_027.py
+++ Euler_027.py
@@ -17,6 +17,7 @@ Find the product of the coefficients, a and b, for the quadratic expression that
 
 import datetime
 import itertools
+import math
 
 from caching import Cache
 from tqdm import tqdm
@@ -27,39 +28,61 @@ start_time = datetime.datetime.now()
 result = "Result"
 
 
-def chain(a: int, b: int):
-    def quadratic(n):
-        return n ** 2 + a * n + b
+def chain(a: int, b: int, cur_max=0):
+    if a % b == 0 and b < cur_max:
+        return 0
 
-    for n in range(10 ** 10):
-        if not is_prime(quadratic(n)):
+    for n in range(10 ** 4):
+        if not is_prime(n ** 2 + a * n + b):
             return n
     return 0
 
 
+def prime_list(upper_bound):
+    primes = [2] + list(range(3, upper_bound, 2))
+    for i in range(upper_bound):
+        primes = list(filter(lambda x: x == primes[i] or x % primes[i] != 0, primes))
+        if i > math.sqrt(upper_bound):
+            return primes
+
+
+primes_test_max = 10 ** 5
+primes_test = prime_list(primes_test_max)
+
+
 @Cache(maxsize=-1)
 def is_prime(number):
-    if number % 2 == 0 or number == 1:
-        return False
-    for test in range(3, int(number / 2), 2):
+    if number <= primes_test_max:
+        return number in primes_test
+    for test in primes_test:
         if number % test == 0:
             return False
+        if test > math.sqrt(number):
+            return True
     return True
 
 
 a_max = 1000
 b_max = 1000
 
-chains = map(lambda x: (chain(x[0], x[1]), x[0], x[1]),
-             itertools.product(range(-a_max, a_max), range(-b_max - 1, b_max + 1)))
-max_chain = (0, 0, 0)
-for test_chain in tqdm(chains, total=a_max * b_max * 4):
-    if test_chain[0] > max_chain[0]: max_chain = test_chain
+# Use primes for b for better runtime
+# if n=0 it tests for b as prime
+# if n=1 a must be uneven, because b and 1 are uneven and the result has to be uneven
+range_a = range(-a_max + 1, a_max, 2)
+range_b = prime_list(b_max)
+range_b += list(map(lambda x: -x, range_b))
+combinations = itertools.product(range_a, range_b)
 
-# key=lambda element: element[2])
-print(max_chain)
+max_chain = (0, 0, 0)
+for combination in tqdm(combinations, total=a_max * len(range_b)):
+    test_chain = (chain(combination[0], combination[1], max_chain[0]), combination[0], combination[1])
+    if test_chain[0] > max_chain[0]:
+        max_chain = test_chain
+
 # End actual code
 end_time = datetime.datetime.now()
 
-print(f'Result is {chain(1, 41)} ({(end_time - start_time)})')
-# Result
+print(f'The quadratic formula n^2 + ({max_chain[1]})*n + ({max_chain[2]})'
+      f' has a chain length of {max_chain[0]}'
+      f' and the factor a*b is {max_chain[1]*max_chain[2]} ({(end_time - start_time)})')
+# The quadratic formula n^2 + (-61)*n + (971) has a chain length of 71 and the factor a*b is -59231 (0:00:09.705505)
